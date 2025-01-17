4dd6771c735a74ca5414bf72e03dabb354038c73
Alozi <alozimozi@gmail.com>
2025-01-17 15:38:31 +0200

    Change logical on components TimerChallenge and ResultModal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,10 @@
 import { useImperativeHandle, useRef } from "react";
 
-export default function ResultModal({ ref, result, targetTime }) {
+export default function ResultModal({ ref, targetTime, timeRemaining, onReset }) {
   const dialog = useRef();
+
+  const userLost = timeRemaining <= 0;
+  const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -12,15 +15,17 @@ export default function ResultModal({ ref, result, targetTime }) {
   });
 
   return (
-    <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{timeRemaining / 1000} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
